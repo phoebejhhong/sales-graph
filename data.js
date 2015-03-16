eedb4a6354bc2ca29593da0980ea0f55ba64633e
Phoebe Hong <phoebejhhong@gmail.com>
2015-03-16 17:26:42 +0900

    add date selection options

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-SalesData = function (filePath) {
+var SalesData = function (filePath) {
   this.filePath = filePath;
 };
 
@@ -15,6 +15,22 @@ SalesData.prototype.getData = function () {
   req.send();
 };
 
+SalesData.prototype.changeData = function (condition) {
+  switch(condition) {
+    case "all":
+      this.currentData = this.data;
+      break;
+    case "last-one-year":
+      this.currentData = this.data.slice(12);
+      break;
+    case "first-one-year":
+      this.currentData = this.data.slice(0,11);
+      break;
+  };
+  d3.select("svg").selectAll("*").remove();
+  this.drawGraph(this.currentData);
+};
+
 SalesData.prototype.drawGraph = function (data) {
 
   var margin = {top: 100, right: 40, bottom: 40, left:80},
@@ -22,21 +38,12 @@ SalesData.prototype.drawGraph = function (data) {
     height = 450,
     barPadding=5;
 
-  var svg = d3.select("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
-
-  var rects = svg.selectAll("rect")
-    .data(data);
-
-  var maxCount = d3.max(data, function(d, i) {
-    return d.count;
-  });
-
+  // get max sales value
   var maxSales = d3.max(data, function(d, i) {
     return d.sales;
   });
 
+  // set x and y scale
   var x = d3.time.scale()
     .domain([
       new Date(Date.parse(data[0].label)),
@@ -48,59 +55,53 @@ SalesData.prototype.drawGraph = function (data) {
     .domain([0, maxSales])
     .range([height, 0]);
 
+  // create svg
+  var svg = d3.select("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom);
+
+  var rects = svg.selectAll("rect")
+  .data(data);
+
+  // create and append x and y axis
   var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .ticks(d3.time.months, 0)
-    .tickFormat(d3.time.format("%b"))
-    .tickSize(10)
-    .tickPadding(15);
+  .scale(x)
+  .orient("bottom")
+  .ticks(d3.time.months, 2)
+  .tickFormat(d3.time.format("%b"))
+  .tickSize(0)
+  .tickPadding(15);
 
   var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickSize(10)
-    .tickPadding(15);
+  .scale(y)
+  .orient("left")
+  .tickSize(0)
+  .tickPadding(15);
 
   svg.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
-    .call(xAxis);
+  .attr("class", "x-axis")
+  .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+  .call(xAxis);
 
   svg.append("g")
-    .attr("class", "y-axis")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("$");
+  .attr("class", "y-axis")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+  .call(yAxis)
+  .append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  .text("$");
 
-  var newRects = rects.enter();
+  // create and append groups for rects
+  var newRects = rects.enter()
+    .append("g")
+    .attr("class", "bar-group");
 
-  var showTooltip = function () {
-    // get the values of this bar
-    var rect = d3.select(this),
-     x = Number(rect.attr("x")) - 10,
-     y = Number(rect.attr("y")) - 70,
-     values = rect.data()[0]
-
-    d3.select(".tooltip")
-      .style("display", "block")
-      .style("left", x + "px")
-      .style("top", y + "px")
-      .html("count: " + values.count + "<br /> sales: " + values.sales + "<br /> fees: " + values.fees);
-  };
-
-  var hideTooltip = function () {
-    d3.select(".tooltip")
-    .attr("style", "display: none")
-  };
-
+  // bars for fees on the top
   newRects.append("rect")
-  .attr("class", "bar")
+  .attr("class", "fee-bar")
   .attr("x", function(d) {
     return margin.left + x(new Date(d.label));
   })
@@ -108,10 +109,44 @@ SalesData.prototype.drawGraph = function (data) {
     return y(d.sales) + margin.top;
   })
   .attr("height", function(d) {
-    return height - y(d.sales);
+    return height - y(d.fees);
   })
   .attr("width", width / data.length - barPadding)
-  .on("mouseover", showTooltip)
+
+  var showTooltip = function () {
+    var rect = d3.select(this).selectAll("rect"),
+    // get the values of this bar
+      x = Number(rect.attr("x")) - 10,
+      y = Number(rect.attr("y")) - 50,
+      values = rect.data()[0];
+
+    d3.select(".tooltip")
+    .style("display", "block")
+    .style("left", x + "px")
+    .style("top", y + "px")
+    .html("count: " + values.count + "<br /> sales: " + values.sales + "<br /> fees: " + values.fees);
+  };
+
+  var hideTooltip = function () {
+    d3.select(".tooltip")
+    .attr("style", "display: none")
+  };
+
+  // bars for sales minus fees on the bottom
+  newRects.append("rect")
+  .attr("class", "bar")
+  .attr("x", function(d) {
+    return margin.left + x(new Date(d.label));
+  })
+  .attr("y", function(d) {
+    return y(d.sales - d.fees) + margin.top;
+  })
+  .attr("height", function(d) {
+    return height - y(d.sales - d.fees);
+  })
+  .attr("width", width / data.length - barPadding)
+
+  newRects.on("mouseover", showTooltip)
   .on("mouseout", hideTooltip);
 
 };
